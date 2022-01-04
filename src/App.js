745ea1d51e306aa78f005f38b20809aeed66c9dc
Aldhair Vera <aldhairvera15@gmail.com>
2022-01-04 18:45:07 -0500

    fix: tasks working

--- src/App.js
+++ src/App.js
@@ -3,19 +3,44 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: [
+        "Sacar la ropa",
+        "Hacer la cama",
+        "Leer un rato"
+      ],
+      todoContent: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ todoContent: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    const todoCopy = [...this.state.todoList];
+    todoCopy.push(this.state.todoContent);
+    this.setState({ todoList: todoCopy, todoContent: "" });
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {
+              this.state.todoList.map((value, index) => {
+                return <li key={index}>{value}</li>
+              })
+            }
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" id="new-task" value={this.state.todoContent} placeholder="Ingresa una tarea y oprime Enter" onChange={this.handleChange} />
+          </form>
         </div>
       </div>
     )
